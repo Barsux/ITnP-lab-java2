0d96177858912639030c856e1b6c24bf8e077f3e
Barsux <yity.r7@gmail.com>
2022-10-12 22:34:31 +0300

    Теперь Point3d наследуется от Point2d

--- src/Point3d.java
+++ src/Point3d.java
@@ -1,49 +1,33 @@
-//Класс трёхмерной точки в пространстве.
-public class Point3d {
-    //Объявляем переменные координат привантно, чтобы к ним не было доступа вне класса
-    private double x, y, z;
-
-    //Конструктор класса
+//Класс трёхмерной точки в пространстве наследуемый от двумерной точки
+public class Point3d extends Point2d {
+    //Объявляем переменные координат привантно, чтобы к ним не было доступа вне класса. Объявляем ещё раз, потому что они не наследовались от двумерной.
+    private double xCoord;
+    private double yCoord;
+    private double zCoord;
+    //Конструктор класса тоже делаем заново
     Point3d(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        this.xCoord = x;
+        this.yCoord = y;
+        this.zCoord = z;
     }
-
     //Базовый конструктор класса
     Point3d(){
         this(0.0, 0.0, 0.0);
     }
-
-    //Геттеры
-    public double getX() {
-        return x;
-    }
-    public double getY() {
-        return y;
-    }
+    //Новый геттер
     public double getZ() {
-        return z;
+        return zCoord;
     }
-
-    //Сеттеры
-    public void setX(double x) {
-        this.x = x;
-    }
-    public void setY(double y) {
-        this.y = y;
-    }
+    //Новый сеттер
     public void setZ(double z) {
-        this.z = z;
+        this.zCoord = z;
     }
-
     //Сравнивает объект с другим объектом, подаваемым через аргумент, по значению.
     public boolean compare(Point3d point){
-        return (this.x == point.x && this.y == point.y && this.z == point.z);
+        return (this.xCoord == point.xCoord && this.yCoord == point.yCoord && this.zCoord == point.zCoord);
     }
-
     //Возвращает геометрическое расстояние между двумя точками.
     public double distanceTo(Point3d point){
-        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2) + Math.pow(this.z - point.z, 2));
+        return Math.sqrt(Math.pow(this.xCoord - point.xCoord, 2) + Math.pow(this.yCoord - point.yCoord, 2) + Math.pow(this.zCoord - point.zCoord, 2));
     }
 }
